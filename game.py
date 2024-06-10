34f5d024583303a73c4c7a4b2591b211c939ab36
HollieMorrison <104519409+HollieMorrison@users.noreply.github.com>
2024-06-10 13:39:22 +0100

    basic structure and functions given

--- game.py
+++ game.py
@@ -1,3 +1,16 @@
+
+
 import random
 from vocab import random_words
 
+def get_word():
+  word = random.choice()
+  return word.upper()
+
+
+def run(word):
+  word_complete = "_" * len(word)
+  guesses = False
+  letters_guessed = []
+  words_guessed = []
+  lives = 6
